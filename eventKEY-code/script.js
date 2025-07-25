471e9d7085b5d3e8548e21dea0c2a01bde2a6804
Nati Melkamu <natimelkemu@gmail.com>
2025-07-25 13:49:16 -0700

    keyboard reader

---
+++ eventKEY-code/script.js
@@ -0,0 +1,13 @@
+const insert = document.querySelector('.insert');
+
+
+window.addEventListener('keydown', (e) => {
+    insert.innerHTML = `<div class="key">
+        ${e.key === ' ' ? 'Space' : e.key}
+        <span class="code">${e.keyCode}</span>
+
+        <span class="key">${e.code}</span>
+        <span class="key">${e.location}</span>
+        <span class="key">${e.repeat}</span>       </div>`;
+   
+})
