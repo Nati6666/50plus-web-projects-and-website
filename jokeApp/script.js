4897426dd78955b9c0a38a4f4c0ad17c63619ab2
Nati Melkamu <natimelkemu@gmail.com>
2025-07-25 12:23:45 -0700

    in simplest way for the seek of simplicity

--- jokeApp/script.js
+++ jokeApp/script.js
@@ -7,20 +7,27 @@ jokeBtn.addEventListener('click', generateJoke);
 
 generateJoke();
 
-function generateJoke() {
+async function generateJoke() {
     const config = {
         headers: {
             Accept: 'application/json',
         },
     };
 
-    fetch('https://icanhazdadjoke.com/', config)
-        .then((res) => res.json())
-        .then((data) => {
-            jokeEl.innerHTML = data.joke;
-        })
-        .catch((err) => {
-            console.error('Error fetching joke:', err);
-            jokeEl.innerHTML = 'Oops! Could not fetch a joke at this time.';
-        });
+    const res = await fetch('https://icanhazdadjoke.com/', config)
+    const data = await res.json();
+    jokeEl.innerHTML = data.joke;
+
+
+        // Uncomment the following lines if you want to fetch a joke from an API
+        
+
+        // .then((res) => res.json())
+        // .then((data) => {
+        //     jokeEl.innerHTML = data.joke;
+        // })
+        // .catch((err) => {
+        //     console.error('Error fetching joke:', err);
+        //     jokeEl.innerHTML = 'Oops! Could not fetch a joke at this time.';
+        // });
 }   
